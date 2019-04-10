7751e7642102e74066de150c6012622d5e298a1e
AxYleN <jeka2644@gmail.com>
2019-04-10 23:59:07 +0300

    Исправлена вёрстка

--- src/Containers/Chat/Chat.jsx
+++ src/Containers/Chat/Chat.jsx
@@ -9,22 +9,15 @@ import MessageInput from '../../Components/MessageInput/MessageInput';
 export default function Chat(props) {
   const { socket, logout, user, userCount } = props;
   const [messages, setMessages] = useState([]);
-  const [text, setText] = useState('');
 
   function addMessage(message) {
     setMessages(m => [...m, message]);
   }
 
-  function handleSendMessage(e) {
-    e.preventDefault();
-
-    if (text === '') return;
-
+  function handleSendMessage(text) {
     const newMessage = {
       text,
     };
-
-    setText('');
 
     socket.emit('message', newMessage);
 
@@ -46,16 +39,12 @@ export default function Chat(props) {
   }, [socket]);
 
   return (
-    <div className="chat-wrapper">
+    <>
       <Header logout={logout} userCount={userCount} />
       <Container className="chat-container">
         <MessageList messages={messages} />
-        <MessageInput
-          text={text}
-          setText={setText}
-          sendMessage={handleSendMessage}
-        />
+        <MessageInput sendMessage={handleSendMessage} />
       </Container>
-    </div>
+    </>
   );
 }
