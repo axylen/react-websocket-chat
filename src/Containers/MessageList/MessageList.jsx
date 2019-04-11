2a092476f9c8b203ddaa21210a176d6d4bfca8be
AxYleN <jeka2644@gmail.com>
2019-04-11 16:35:06 +0300

    Добавлена кнопка для автопрокрутки вниз

--- src/Containers/MessageList/MessageList.jsx
+++ src/Containers/MessageList/MessageList.jsx
@@ -1,8 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import './MessageList.css';
 
+import ReactSVG from 'react-svg';
+import Arrow from '../../arrow.svg';
 import Message from '../../Components/Message/Message';
 const Msg = React.memo(Message);
 
@@ -10,9 +12,10 @@ export default function MessageList(props) {
   const messages = props.messages;
   const isFirstRender = usePrevious(messages, []).length < 1;
   const list = useRef(null);
+  const [showBtn, setShowBtn] = useState(false);
 
   useEffect(() => {
-    const el = list.current.querySelector('.message-list__content>:last-child');
+    const el = getLastInList();
 
     if (!el) return;
 
@@ -20,15 +23,34 @@ export default function MessageList(props) {
     const scrollHeight = list.current.clientHeight;
     const offset = top - scrollHeight;
 
-    if (offset < 250) {
-      el.scrollIntoView({ behavior: 'auto', block: 'start' });
-    } else if (isFirstRender) {
-      el.scrollIntoView({ behavior: 'auto' });
+    if (offset < 250 || isFirstRender) {
+      el.scrollIntoView();
     }
   }, [messages]);
 
+  function getLastInList() {
+    return list.current.querySelector('.message-list__content>:last-child');
+  }
+
+  function handleScroll(e) {
+    const list = e.target;
+    const el = getLastInList();
+
+    if (!el) return;
+
+    const top = el.getBoundingClientRect().top;
+    const scrollHeight = list.clientHeight;
+    const offset = top - scrollHeight;
+
+    if (offset > 250 && !showBtn) {
+      setShowBtn(true);
+    } else if (offset <= 250 && showBtn) {
+      setShowBtn(false);
+    }
+  }
+
   return (
-    <div className="message-list" ref={list}>
+    <div className="message-list" ref={list} onScroll={handleScroll}>
       <TransitionGroup className="message-list__content">
         {messages.map(el => (
           <CSSTransition
@@ -39,6 +61,15 @@ export default function MessageList(props) {
           </CSSTransition>
         ))}
       </TransitionGroup>
+      {showBtn ? (
+        <button
+          className="to-bottom"
+          onClick={() =>
+            getLastInList().scrollIntoView({ behavior: 'smooth' })
+          }>
+          <ReactSVG src={Arrow} svgClassName="to-bottom__arrow" />
+        </button>
+      ) : null}
     </div>
   );
 }
